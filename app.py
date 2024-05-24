e0c876a53d2611b9eb97889eb8a107dbbc9cf829
TimothyJNeale <sixhillsai@icloud.com>
2024-05-24 19:43:31 +0100

    Finished date navigation functionality

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 
 habits = ["Test Habit", "Test habit 2"]  # List of habits
 
-def date_range(start: datetime.date):
-    dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
-    return dates
+@app.context_processor
+def add_calc_data_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+        return dates
+    
+    return {"date_range": date_range}
 
 @app.route("/")
 def index():
@@ -16,12 +20,11 @@ def index():
         selected_date = datetime.date.fromisoformat(date_str)
     else:
         selected_date = datetime.date.today()
-    print(selected_date)
-    
+    #print(selected_date)
+
     return render_template("index.html", 
                            habits=habits, 
                            title="Habit Tracker - Home", 
-                           date_range=date_range, 
                            selected_date=selected_date
                            )
 
@@ -31,4 +34,7 @@ def add_habit():
     if request.method == "POST":
         habits.append(request.form.get("habit"))
             
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+    return render_template("add_habit.html", 
+                           title="Habit Tracker - Add Habit", 
+                           selected_date=datetime.date.today()
+                           )
