7358e6d6a2be405a4d3d21151a87fa044c2a0336
TimothyJNeale <sixhillsai@icloud.com>
2024-05-24 16:38:02 +0100

    Added date menu structure

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 habits = ["Test Habit", "Test habit 2"]  # List of habits
 
+def date_range(start: datetime.date):
+    dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+    return dates
+
 @app.route("/")
 def index():
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+    print(selected_date)
+    
+    return render_template("index.html", 
+                           habits=habits, 
+                           title="Habit Tracker - Home", 
+                           date_range=date_range, 
+                           selected_date=selected_date
+                           )
 
 
 @app.route("/add", methods=["GET", "POST"])
