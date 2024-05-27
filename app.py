269517cb8d00cd06eb55f5b0bc5e4f1a36328554
TimothyJNeale <sixhillsai@icloud.com>
2024-05-27 16:11:08 +0100

    Integrated with MongoDB

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from pymongo import MongoClient
 
 load_dotenv()
 
-
 def create_app():
     app = Flask(__name__)
     client = MongoClient(os.getenv("MONGODB_URI"))
-    app.db = client.habit_tracker
+    app.db = client.get_default_database()
+    #app.db = client.habit_tracker
     #print(client.list_database_names())
 
     app.register_blueprint(pages)
